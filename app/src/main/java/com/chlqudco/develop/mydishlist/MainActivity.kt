14175ae0cd18b8d6d0f170b64bdf65eed673637e
chlqudco <chlqudco2@naver.com>
2022-08-03 00:14:19 +0900

    1. 앱 이름 변경 2. night 테마 삭제 및 다크 모드 금지 3. NoActionBar 테마 지정 4. 메인화면 UI 작업

--- app/src/main/java/com/chlqudco/develop/mydishlist/MainActivity.kt
+++ app/src/main/java/com/chlqudco/develop/mydishlist/MainActivity.kt
@@ -2,10 +2,14 @@ package com.chlqudco.develop.mydishlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //다크모드 금지
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
     }
 }
